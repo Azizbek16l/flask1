8907b8686fb074ecc290d053585f8a6b1a0566b6
Azizbek Topilboyev <azizbektopilboyev7@gmail.com>
2026-02-19 14:30:16 +0500

    black ishga tushdi

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
-app=Flask(__name__)
 
-@app.route('/')
+app = Flask(__name__)
+
+
+@app.route("/")
 def home():
     return "salom dev"
-if __name__=="__main__":
-    app.run(port=4200, debug=True)
-    
+
+
+if __name__ == "__main__":
+    app.run(port=4200, debug=False)
