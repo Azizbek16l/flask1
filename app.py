40b76c6d4d050d35c3228e0db6848f1c9d83ceed
Azizbek Topilboyev <azizbektopilboyev7@gmail.com>
2026-02-20 23:08:50 +0500

    added method and change controllers

--- app.py
+++ app.py
@@ -1,35 +1,25 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-   return render_template('add.html')
+    return render_template("add.html")
 
-@app.route('/add')
+
+@app.route("/add",methods=["POST","GET"])
 def adder():
-    a=request.args.get('a','')
-    b=request.args.get('b','')
-    
-    a=int(a)
-    b=int(b)
-    
+    a = request.form.get("a", "")
+    b = request.form.get("b", "")
 
-    result=a+b
-    return render_template('result.html',result=result)
+    a = int(a)
+    b = int(b)
 
+    result = a + b
+    return render_template("result.html", result=result)
 
 
 
-@app.route('/min')
-def  minus():
-    c=request.args.get('c','')
-    d=request.args.get('d','')
-    
-    c=int(c)
-    d=int(d)
-    
-    return f'minus : {c-d}'
 if __name__ == "__main__":
     app.run(port=4200, debug=True)
